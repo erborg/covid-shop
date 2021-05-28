5a8065c0e58d0762984b70271fa74be3e258b0b5
Erik Borg <e.borg1990@gmail.com>
2021-05-28 16:09:01 +0300

    change spinner

--- src/Shop/index.tsx
+++ src/Shop/index.tsx
@@ -15,6 +15,6 @@ export default function Shop() {
   const item = items.map((object, index) => {
     return <Item key={index} info={object} />;
   });
-  const loading = <Loader type="Puff" color="#00BFFF" height={100} width={100} />;
+  const loading = <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />;
   return <div className="shop">{loader ? loading : item}</div>;
 }
